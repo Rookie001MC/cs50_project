52fb5c9a5a603e4cf3ea130c07552a47408f59cf
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2022-08-04 23:21:34 +0700

    Implemented utilities and removed debug code.
    Also documented some functions.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
 import os
 
-from flask import Flask, Response, request
+import requests
+from flask import Flask, Response, render_template, request
 
 app = Flask(__name__)
 
@@ -11,11 +12,14 @@ access_token = os.getenv("ACCESS_TOKEN", None)
 
 @app.route("/webhook", methods=["GET"])
 def webhook_verify():
+    """
+    Webhook verification for the bot.
+    https://developers.facebook.com/docs/messenger-platform/webhooks
+    """
+
     mode = request.args.get("hub.mode")
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
-
-    print(f"{mode}\n{token}\n{challenge}")
 
     if mode and token:
         if mode == "subscribe" and token == verify_token:
@@ -27,13 +31,91 @@ def webhook_verify():
 
 @app.route("/webhook", methods=["POST"])
 def webhook_handler():
+    """
+    Handle message events sent to the bot.
+    """
     body = json.loads(request.data.decode("utf-8"))
 
     if body["object"] == "page":
+        for entry in body["entry"]:
+            webhook_event = entry["messaging"][0]
+            sender_psid = webhook_event["sender"]["id"]
+
+            if webhook_event["message"]:
+                handle_message(sender_psid, webhook_event["message"])
+            elif webhook_event["postback"]:
+                handle_postback(sender_psid, webhook_event["postback"])
         return Response(status=200, response="EVENT_RECEIVED")
     else:
         return Response(status=404)
 
+
+@app.route("/profile")
+def profile_setup():
+    """
+    Setup the bot using the Messenger Profile API.
+    """ 
+
+    profile_url = f"https://graph.facebook.com/v14.0/me/messenger_profile?access_token={access_token}"
+    profile = {
+        "data": [
+            {
+                "greeting": [
+                    {
+                        "locale": "default",
+                        "text": "This is a simple Chatbot, written in Flask, for CS50 Python.",
+                    },
+                    {
+                        "locale": "vi_VN",
+                        "text": "Đây là 1 con Chatbot, được viết bằng Flask, dành cho dự án của khoá học CS50 Python.",
+                    },
+                ],
+                "get_started": {"payload": "GET_STARTED_PAYLOAD"},
+            }
+        ]
+    }
+    try:
+        r = requests.post(url=profile_url, json=profile)
+
+        if r.status_code == 200:
+            return f"<p>Successfully setup profile!</p>"
+        elif r.status_code > 500:
+            print(f"Request failed: {r.text}")
+            return f"<p>Profile setup failed! Please check console for details.</p>"
+
+    except requests.exceptions.ConnectionError as err:
+        print(f"No connection: {err}")
+
+
+
+
+# Utilities go here.
+
+def handle_message():
+    pass
+
+def handle_postback():
+    pass
+
+def call_sendAPI(sender_psid, response):
+    graphapi_url = f"https://graph.facebook.com/v14.0/me/messages?access_token={access_token}"
+    payload = {
+        "recipient": {
+            "id": sender_psid,
+        },
+        "message": {
+            "text": response,
+        }
+    }
+
+    try:
+        r = requests.post(graphapi_url, json=payload)
+
+    except requests.exceptions.HTTPError as err:
+        print(f"Message failed to send: {err}")
+        return Response(response=err, status=r.status_code)
+    except requests.exceptions.ConnectionError as err:
+        print(f"No connection: {err}")
 
 if __name__ == "__main__":
     app.run(debug=True)
