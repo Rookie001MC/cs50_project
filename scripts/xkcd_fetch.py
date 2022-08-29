1e02d726a2079b0f125fd7c0273f4819e098921b
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2022-08-29 23:11:25 +0700

    Added documentation, refactored code.

--- scripts/xkcd_fetch.py
+++ scripts/xkcd_fetch.py
@@ -2,46 +2,61 @@ import xkcd
 
 
 def fetcher(user_input):
+    """Process the inputted command and fetches the requested comic.
+
+    Args:
+        user_input (str): the command as inputted by user.
+
+    Returns:
+        dict: a response object to be sent back to the `handle_message` function for processing and sending to FB.
+    """
     commands = user_input.split(" ")
     print(commands)
     if len(commands) == 1 or commands[1] == "latest":
         latest = xkcd.getLatestComic()
-        info = info_getter(latest)
+        comic_url = get_image_url(latest)
     elif commands[1] == "random":
         random = xkcd.getRandomComic()
-        info = info_getter(random)
+        comic_url = get_image_url(random)
     else:
         try:
             specific = xkcd.getComic(int(commands[1]))
-            info = info_getter(specific)
+            comic_url = get_image_url(specific)
         except ValueError:
-            info = "Invalid comic number or subcommand!"
+            comic_url = "Invalid comic number or subcommand!"
 
-    if len(info) == 2:
+    if comic_url:
         response = {
             "attachment": {
                 "type": "image",
                 "payload": {
-                    "url": info[1],
+                    "url": comic_url,
                 },
             },
         }
     else:
         response = {
-            "text": info,
+            "text": comic_url,
         }
 
     return response
 
 
-def info_getter(comic):
+def get_image_url(comic):
+    """Fetches the image of a given comic.
+
+    Args:
+        comic (object): An `xkcd.Comic` object as requested by the user.
+
+    Returns:
+        str: The url of the comic.
+    """
     try:
-        alt_text = comic.getAltText()
         image_url = comic.getImageLink()
     except AttributeError:
         return "An error occured, there may be a connection error, or you must have given a non-existant comic!"
     else:
-        return [alt_text, image_url]
+        return image_url
 
 
 """
