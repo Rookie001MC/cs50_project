c3cfc0dffc2c863fe86614ecf433f92004519227
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2022-08-30 19:25:15 +0700

    Added tests, removed legacy code, credited idea
    Without the CS50's staff answer generation, I definitely won't be able to get the test_random to work.

--- tests/test_xkcd_fetch.py
+++ tests/test_xkcd_fetch.py
@@ -5,6 +5,9 @@ from scripts.xkcd_fetch import fetcher
 
 
 def test_random(monkeypatch):
+    # Sometimes looking at how they mark your problem sets actually works.
+    # Idea for monkeypatching the randomizer:
+    # https://github.com/cs50/problems/blob/2022/python/game/testing.py
     monkeypatch.setattr("random.randint", lambda a, b: 20)
     out_response = fetcher(f"/xkcd random")
 
@@ -22,21 +25,12 @@ def test_latest():
 
 
 def test_specific():
-    comic_num = 646
+    comic_num = random.randint(1, xkcd.getLatestComicNum())
     out_response = fetcher(f"/xkcd {comic_num}")
-
+    print(out_response)
     image_url = out_response["attachment"]["payload"]["url"]
 
     assert image_url == xkcd.getComic(comic_num).getImageLink()
-
-
-# def test_random():
-#    random.seed(1)
-#    out_response = fetcher(f"/xkcd random")
-#
-#    image_url = out_response["attachment"]["payload"]["url"]
-#
-#   assert image_url == xkcd.getRandomComic().getImageLink()
 
 
 def test_out_of_bounds():
@@ -44,7 +38,11 @@ def test_out_of_bounds():
 
     err_message = out_response["text"]
 
-    assert (
-        err_message
-        == "An error occured, there may be a connection error, or you must have given a non-existant comic!"
-    )
+    assert err_message == "Your requested comic, number 9999, does not exist!"
+
+
+def test_invalid_subcommand():
+    out_response = fetcher(f"/xkcd invalid")
+
+    err_message = out_response["text"]
+    assert err_message == "Invalid comic number or subcommand!"
