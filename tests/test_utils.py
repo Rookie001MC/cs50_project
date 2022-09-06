75eea9858678a889decf8d92c2bb7ce57b4f5db1
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2022-09-06 20:45:37 +0700

    Fixed command object output, escaped characters

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,26 +4,31 @@ import utils
 def test_main_no_action(capfd):
     utils.main()
     out, err = capfd.readouterr()
-    assert out == "There's nothing here ¯\_(ツ)_/¯\n"
+    assert out == "There's nothing here ¯\\_(ツ)_/¯\n"
 
 
 def test_handle_message_command():
     request_object = {"text": "/help"}
     response = utils.handle_message(request_object)
     assert response == {
-        "text": f"Available commands:\n\n - /xkcd [random/latest/(any number)]: Gets a XKCD webcomic 📖. Leave blank for the latest comic.\n- /weather <city>: Show current weather for the provided city ☀️."
+        "text": f"""Available commands:
+
+- /xkcd [random/latest/(any number)]: 📖 Gets a XKCD webcomic. Leave blank for the latest comic.
+- /weather <city>: 🌥️ Gets the current weather of a given city. \nThe format must be (City name, Country in 2 letters.)
+- /fuel: ⛽ Gets the current price of common fuel products in Vietnam.
+                        """
     }
 
 
 def test_handle_message_invalid_command():
     request_object = {"text": "/invalid"}
     response = utils.handle_message(request_object)
-    assert response == {"text": "Sorry, but this command does not exist.\n¯\_(ツ)_/¯"}
+    assert response == {"text": "Sorry, but this command does not exist.\n¯\\_(ツ)_/¯"}
 
 
 def test_handle_message_deny_normal_text():
     request_object = {"text": "oooga booga"}
     response = utils.handle_message(request_object)
     assert response == {
-        "text": f"If you're trying to send me some random text, I'm sorry, cause I'm dumb.\n¯\_(ツ)_/¯"
+        "text": f"If you're trying to send me some random text, I'm sorry, cause I'm dumb.\n¯\\_(ツ)_/¯"
     }
